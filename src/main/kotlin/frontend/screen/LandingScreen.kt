31db5eb21a73c7de151cd6c0212bbc1c1fd3be4e
verityyt <joshua-schmidt@online.de>
2021-01-17 16:35:08 +0100

    :construction: Add ButtonWidget (+ two buttons in LandingScreen)

--- src/main/kotlin/frontend/screen/LandingScreen.kt
+++ src/main/kotlin/frontend/screen/LandingScreen.kt
@@ -2,6 +2,9 @@ package frontend.screen
 
 import frontend.Screen
 import frontend.utils.CustomFont
+import frontend.widgets.ButtonWidget
+import frontend.widgets.Widget
+import java.awt.Color
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.image.ImageObserver
@@ -10,20 +13,30 @@ import javax.imageio.ImageIO
 
 class LandingScreen : Screen() {
 
+    private val installButton = ButtonWidget(230, 660, 350, 60, 25, "Install", 3, 30, true) { }
+
+    private val uninstallButton = ButtonWidget(230, 590, 350, 60, 25, "Uninstall", 3, 30, false) { }
+
+    override val widgets: List<Widget> = listOf(installButton, uninstallButton)
+
     override fun paint(g: Graphics, g2: Graphics2D, observer: ImageObserver) {
 
         g.drawImage(ImageIO.read(File("files/images/Rocket.png")), 300, 30, 200, 205, observer)
 
+        g.color = Color.black
         g.font = CustomFont.medium?.deriveFont(24f)
         g.drawString("Welcome to Hardware Monitoring Display,", 170, 315)
 
         g.font = CustomFont.light?.deriveFont(24f)
-        g.drawString("the next generation hardware monitoring software, with useful",80, 350)
-        g.drawString("features and customizable userinterface. You’re able to change",80, 380)
-        g.drawString("every single color of the userinterface to give it your own style.",80, 410)
-        g.drawString("Furthermore you can set alarms so that if you lose sight of your",80, 440)
-        g.drawString("temperatures, you will still notice if any part of your computer",80, 470)
-        g.drawString("reaches a critical temperature.",80, 500)
+        g.drawString("the next generation hardware monitoring software, with useful", 80, 350)
+        g.drawString("features and customizable userinterface. You’re able to change", 80, 380)
+        g.drawString("every single color of the userinterface to give it your own style.", 80, 410)
+        g.drawString("Furthermore you can set alarms so that if you lose sight of your", 80, 440)
+        g.drawString("temperatures, you will still notice if any part of your computer", 80, 470)
+        g.drawString("reaches a critical temperature.", 80, 500)
+
+        installButton.paint(g, g2, observer)
+        uninstallButton.paint(g, g2, observer)
 
     }
 
---
+++ src/main/kotlin/frontend/widgets/ButtonWidget.kt
@@ -0,0 +1,102 @@
+package frontend.widgets
+
+import frontend.utils.CustomFont
+import java.awt.*
+import java.awt.geom.RoundRectangle2D
+import java.awt.image.ImageObserver
+
+class ButtonWidget(
+    private val x: Int,
+    private val y: Int,
+    private val w: Int,
+    private val h: Int,
+    private val arc: Int,
+    private val text: String,
+    private val stroke: Int,
+    private val fontSize: Int,
+    private val available: Boolean,
+    private val action: () -> Unit
+) : Widget() {
+
+    var hovered = false
+
+    override fun paint(g: Graphics, g2: Graphics2D, observer: ImageObserver) {
+        if (!available) {
+            g2.paint = Color.decode("#BFBFBF")
+            g2.stroke = BasicStroke(stroke.toFloat())
+            g2.draw(
+                RoundRectangle2D.Float(
+                    x.toFloat(),
+                    y.toFloat(),
+                    w.toFloat(),
+                    h.toFloat(),
+                    arc.toFloat(),
+                    arc.toFloat()
+                )
+            )
+
+            CustomFont.drawCentredString(
+                g,
+                Rectangle(x, y, w, h),
+                text,
+                Color.decode("#BFBFBF"),
+                CustomFont.regular!!.deriveFont(fontSize.toFloat())
+            )
+        } else {
+            if (hovered) {
+                g2.paint = Color.black
+                g2.stroke = BasicStroke(stroke.toFloat())
+                g2.fill(
+                    RoundRectangle2D.Float(
+                        x.toFloat(),
+                        y.toFloat(),
+                        w.toFloat(),
+                        h.toFloat(),
+                        arc.toFloat(),
+                        arc.toFloat()
+                    )
+                )
+
+                CustomFont.drawCentredString(
+                    g,
+                    Rectangle(x, y, w, h),
+                    text,
+                    Color.white,
+                    CustomFont.regular!!.deriveFont(fontSize.toFloat())
+                )
+            } else {
+                g2.paint = Color.black
+                g2.stroke = BasicStroke(stroke.toFloat())
+                g2.draw(
+                    RoundRectangle2D.Float(
+                        x.toFloat(),
+                        y.toFloat(),
+                        w.toFloat(),
+                        h.toFloat(),
+                        arc.toFloat(),
+                        arc.toFloat()
+                    )
+                )
+
+                CustomFont.drawCentredString(
+                    g,
+                    Rectangle(x, y, w, h),
+                    text,
+                    Color.black,
+                    CustomFont.regular!!.deriveFont(fontSize.toFloat())
+                )
+            }
+        }
+    }
+
+    override fun click(x: Int, y: Int) {
+        if (x > this.x && x < (this.x + this.w) && y > this.y && this.y < (this.y + h) && available) {
+            action()
+        }
+    }
+
+    override fun hover(x: Int, y: Int) {
+        hovered = (x > this.x && x < (this.x + this.w) && y > this.y && this.y < (this.y + h))
+    }
+
+}
