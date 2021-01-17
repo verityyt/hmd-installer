e24005c6193464f00265e3cd38c72003c1c252d0
verityyt <joshua-schmidt@online.de>
2021-01-17 12:56:37 +0100

    :construction: Add LandingScreen

--- src/main/kotlin/HMDInstaller.kt
+++ src/main/kotlin/HMDInstaller.kt
@@ -1,10 +1,12 @@
-import backend.UninstallationManager
+import frontend.Window
 
 object HMDInstaller {
 
+    val version = "2.1.1"
+
     @JvmStatic
     fun main(args: Array<String>) {
-        UninstallationManager.startUninstallation()
+        Window.build()
     }
 
 }
